65863656e77a24e5cf3e512c863fa3cbccd5c4fb
Amr <bawzeeramr@gmail.com>
2024-01-11 13:15:22 +0000

    x

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-// write a new function game()
-    // use old function inside of game()
-    // game is played to the best of 5
-    // keeps score
-    // winner or loser is declared at the end
-// use console.log() to display the results of each round and the winner at the end
-
-
 // declare variable computerSelection and playerSelection
-const playerSelection = playerPicked(); // executed first
-const computerSelection = getComputerChoice(); // executed second
+let playerSelection = playerPicked(); // executed first
+let computerSelection = getComputerChoice(); // executed second
+let computerScore=0;
+let playerScore=0;
+let count=1;
 
 // create function getComputerChoice() that randomly returns rock paper or scissors and assign to computerSelection
 function getComputerChoice() {
@@ -48,15 +43,55 @@ function playerPicked() {
     // playRound declares the winner by returning a string using console.log
     // if a tie happens the round is replayed
 function playRound(computerSelection, playerSelection) {
+
     if (computerSelection === playerSelection) {
-        console.log("It's a tie!")
+        console.log("Round " + count);
+        console.log("You used: " + playerSelection);
+        console.log("Opponent used: " + computerSelection);
+        console.log("It's a tie! Here's a REMATCH!")
     } else if ((computerSelection==="Rock" && playerSelection==="Paper") || (computerSelection==="Paper" && playerSelection==="Scissors") || (computerSelection==="Scissors" && playerSelection==="Rock")) {
+        console.log("Round " + count);
+        console.log("You used: " + playerSelection);
+        console.log("Opponent used: " + computerSelection);
+        count++;
         console.log("You won! " + playerSelection + " beats " + computerSelection);
+        playerScore++;
     } else {
+        console.log("Round " + count);
+        console.log("You used: " + playerSelection);
+        console.log("Opponent used: " + computerSelection);
+        count++;
         console.log("You lost! " + computerSelection + " beats " + playerSelection);
+        computerScore++;
     }
+
 }
 
-console.log(playerSelection);
-console.log(computerSelection);
-playRound(computerSelection, playerSelection); // executed third
+// create a new function
+ // keeps score, plays best of five and declares game winner
+function game() {
+// game is played to best of 5 by seeing if both the scores are less than 3
+    // if score isn't less than 3, then the other player has no chance of winning
+    while ( playerScore < 3 && computerScore < 3 ){
+// uses old function playRound()
+        playRound(computerSelection, playerSelection);
+// keeps score
+        console.log("You: " + playerScore + " " + "Opponent: " + computerScore)
+
+// makes sure that the game doesn't keep on going if one of the players scores 3
+        if ( playerScore < 3 && computerScore < 3 ) {
+        playerSelection = playerPicked();
+        computerSelection = getComputerChoice();
+        }
+
+    }
+
+// winner or loser is declared via console.log()
+    if (playerScore>computerScore) {
+        console.log("You won the game!");
+    } else (
+        console.log("You lost the game :(")
+    )
+}
+
+game();
